0c15319dfbdd67ad1f69d25404a8dfde88ceba33
mariamabdelati <75902279+mariamabdelati@users.noreply.github.com>
2021-02-25 16:53:28 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<string>
+//include header file with class definition here
+
+int main(int argc, char const *argv[]) 
+/*
+int argc is used to count the arguments that the user has inputed
+int argv stores the values in an array which can let us print it if we want to
+*/
+{
+    //there needs to be a function thing for opening the file in order to read it 
+    while (true)
+    {
+        //this reads the items in the file
+        task.read_file();
+
+        //this makes sure there are tasks to display in the txt
+        if (task.get_count() > 0)
+        {
+            task.display();
+            std::cout << std::endl;
+            std::cout << "Below are the features currently supported by the program: \n 1)  
+            Add Task \n 2) Remove Task \n 3) View Tasks \n Please input what you would like to do: "
+            int choice;
+            getline(cin,choice)
+            
+            if (choice == "1" || choice == "1) Add Task" || choice == "Add Task" || choice == "1)")
+			{
+				//function from class runs to add the task
+                task.add();
+			} else if (choice == "2" || choice == "2) Remove Task" || choice == "Remove Task" || choice == "2)") {
+				//function from class runs to remove the task chosen
+                task.remove();
+			} else if (choice == "3" || choice == "3) View Tasks" || choice == "View Tasks" || choice == "3)")) {
+				//function from class runs to view completed tasks and/or tasks to complete
+                task.view();
+			} else {
+				break;
+			}
+
+        }else {
+            //program automatically creates new list
+            std::cout << "creating new list... \n";
+            //function called to create new list
+            list.create();
+            std::cout << std::endl;
+            //new tasks displayed
+            task.display();
+            
+            
+        }
+    }
+    return 0;
+}
