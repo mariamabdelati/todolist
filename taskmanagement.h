703b97e5219fd11bb57670485ec7559c0b52a545
mariamabdelati <75902279+mariamabdelati@users.noreply.github.com>
2021-03-11 00:12:44 +0200

    Add files via upload

---
+++ taskmanagement.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <stdio.h>
+#include "item.h"
+
+using namespace std;
+
+class TaskManagement
+{
+public:
+    TaskManagement();
+    TaskManagement(const char*);
+    ~TaskManagement();
+
+    void read();
+    void display();
+    void create();
+    void write();
+    void add();
+    void del();
+    void view_incomplete();
+    void view_complete();
+    void reset();
+    void check();
+    void uncheck();
+    void set_priority();
+
+    int get_count();
+
+private:
+    const char* filename;
+    vector<Item> list;
+};
