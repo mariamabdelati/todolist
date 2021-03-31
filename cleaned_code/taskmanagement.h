8d08056bc20593355db544c879250ece46ca0f8e
mariamabdelati <75902279+mariamabdelati@users.noreply.github.com>
2021-03-31 09:54:57 +0200

    Create taskmanagement.h

---
+++ cleaned_code/taskmanagement.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include "item.h"
+#include "filemanagement.h"
+
+
+using namespace std;
+
+class TaskManagement : public FileManagement
+{
+public:;
+
+    TaskManagement();
+    TaskManagement(const char *filename);
+    ~TaskManagement();
+
+
+    static void organize(); //static method since it is accessed from within the class and no object requires it
+    void display();
+    void create();
+
+    void add();
+    void del();
+    void edit();
+    void view_incomplete();
+    void view_complete();
+    void check();
+    void uncheck();
+    void set_priority();
+
+    int get_count();
+
+};
