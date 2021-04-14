712d9c0dfc707b47763ffa2df29924529a047b19
mariamabdelati <75902279+mariamabdelati@users.noreply.github.com>
2021-04-14 22:21:14 +0200

    Add files via upload

---
+++ workingDB/taskmanagement.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <list>
+#include <iterator>
+#include "item.h"
+#include "databasemanagement.h"
+
+
+using namespace std;
+
+class TaskManagement : public DatabaseManagement
+{
+public:;
+
+    //TaskManagement();
+    TaskManagement(const char *filename);
+    ~TaskManagement();
+
+
+    static void organize(); //static method since it is accessed from within the class and no object requires it
+    void display();
+    void create();
+
+    void add();
+    void del();
+    void edit();
+    void view_incomplete();
+    void view_complete();
+    void check();
+    void uncheck();
+    void set_priority();
+    bool priority_check();
+    void priority_sort_by_high();
+    void priority_sort_by_low();
+    bool restore_list();
+
+    int get_count();
+    ::list<Item> list1;
+
+};
