b1b95fe83d20feafbcc488ce45e33d0eb6a04c9d
mariamabdelati <75902279+mariamabdelati@users.noreply.github.com>
2021-04-17 18:21:49 +0200

    Add files via upload

---
+++ fixed_date/databasemanagement.cpp
@@ -0,0 +1,155 @@
+#include "databasemanagement.h"
+/*DatabaseManagement::DatabaseManagement()
+{
+    filename = nullptr; //file is null
+}*/
+DatabaseManagement::DatabaseManagement(const char* filename)
+{
+    //this->filename = filename;
+    not_connected = sqlite3_open(filename, &db);
+
+    if( not_connected ) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+
+    } else {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+
+    sqlite3_close(db);
+
+    /*
+    this is a pointer, arrow is used to dereference, helps assign the variable of same name to itself
+    */
+}
+DatabaseManagement::~DatabaseManagement()= default;
+
+/*int createDB(const char* s)
+{
+    sqlite3* db;
+    int exit = 0;
+    exit = sqlite3_open(s, &db);
+
+    sqlite3_close(db);
+}
+
+*/
+string quotesql( const string& s ) {
+    return string("'") + s + string("'");
+}
+/*int DatabaseManagement::connect()
+{
+    not_connected = sqlite3_open(filename, &db);
+
+    if( not_connected ) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return(0);
+    } else {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+
+    sqlite3_close(db);
+
+    return(0);
+}*/
+
+void DatabaseManagement::deleteData()
+{
+    //connect();
+    string sql3 = "DELETE FROM TODO";
+
+    sqlite3* db;
+    try
+    {
+        sqlite3_open("todolist.db", &db);
+
+        char* messageError;
+        int exit = sqlite3_exec(db, sql3.c_str(), nullptr, nullptr, &messageError);
+
+        if (exit != SQLITE_OK)
+        {
+            cerr << "Error Delete" << endl;
+            sqlite3_free(messageError);
+        } else
+        {
+            cout << "Delete Successful" << endl;
+        }
+        sqlite3_close(db);
+    } catch (const exception & e)
+    {
+        cerr << e.what();
+    }
+}
+
+void DatabaseManagement::insertData() {
+
+    deleteData();
+
+    int insert = 0;
+    for (Item item : list)
+    {
+        if (item.text().empty()) continue;
+        else insert++;
+        sql2 = "INSERT INTO TODO (ID, TASK,STATUS,PRIORITY,DUEDATE,CATEGORY) VALUES (" + quotesql(to_string(insert)) + "," + quotesql(item.text()) + "," + quotesql((item.is_done() ? "true" : "false")) + "," + quotesql(item.priority()) + "," + quotesql(item.date()) + "," + quotesql(item.category()) + ");";
+        try
+        {
+            sqlite3_open("todolist.db", &db);
+
+            char* messageError;
+            int exit = sqlite3_exec(db, sql2.c_str(), nullptr, nullptr, &messageError);
+
+            if (exit != SQLITE_OK)
+            {
+                cerr << "Error Insert" << endl;
+                sqlite3_free(messageError);
+            } else
+            {
+                cout << "Insert Successful" << endl;
+            }
+            sqlite3_close(db);
+        } catch (const exception & e)
+        {
+            cerr << e.what();
+        }
+    }
+}
+
+void DatabaseManagement::selectData()
+{
+    sqlite3 *db;
+    sqlite3_stmt *stmt = nullptr;
+
+
+    if (sqlite3_open("todolist.db", &db) == SQLITE_OK)
+    {
+        sqlite3_prepare( db, "SELECT * from TODO;", -1, &stmt, nullptr );//preparing the statement
+        sqlite3_step( stmt );//executing the statement
+        //char * str = (char *) sqlite3_column_text( stmt, 0 ); //reading the 1st column of the result
+    }
+    else
+    {
+        cout << "Failed to open db\n";
+    }
+
+
+    list.clear();
+    while( sqlite3_column_text( stmt, 0 ) )
+    {
+        string line;
+        for( int i = 1; i < 6; i++ )
+        {
+            line += std::string( (char *)sqlite3_column_text( stmt, i ));
+            if (i != 5)
+                line += " ";
+        }
+        //cout <<line << endl;
+        sqlite3_step( stmt );
+        Item item(line);
+        list.emplace_back(line);
+    }
+
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
+}
+
+
+
---
+++ fixed_date/item.cpp
@@ -0,0 +1,260 @@
+#include "item.h"
+
+#include <utility>
+Item::Item()
+{
+    _tasktext = "";
+    _done = false;
+    _priority = "0";
+    _dateDay = "00";
+    _dateMonth = "00";
+    _dateYear = "0000";
+    _date = _dateDay + "/" + _dateMonth + "/" + _dateYear;
+    _category = "cat";
+}
+
+Item::Item(const string& item)
+{
+
+    int startfalse = int(item.find("false"));
+    /*
+    if found, the index for when the string starts is stored
+    if not found, the index stored is -1
+    */
+    int starttrue = int(item.find("true"));
+    string str;
+    string pr;
+    string dt;
+    string ct;
+
+    if (startfalse > -1 && starttrue == -1) // item has false
+    {
+        //this stores the line substring without the word done/not done (subtract one to end right before the index of done)
+        str = item.substr(0, startfalse - 1);
+        _done = false;
+        pr = item.substr(startfalse + 6, startfalse + 7);
+        dt = item.substr(startfalse + 9, startfalse + 18);
+        ct = item.substr(startfalse + 20, startfalse + 23);
+    }
+    else if (starttrue > -1 && startfalse == -1) { // item has true
+        str = item.substr(0, starttrue - 1);
+        _done = true;
+        pr = item.substr(starttrue + 5, starttrue + 6);
+        dt = item.substr(startfalse + 8, startfalse + 17);
+        ct = item.substr(startfalse + 19, startfalse + 22);
+    }
+    else if (startfalse == -1 && starttrue == -1) { // item is user input (no true or false)
+        str = item;
+        _done = false;
+        pr = "0";
+        dt = "00/00/0000";
+        ct = "cat0";
+    }
+
+    _tasktext = str;
+    _priority = pr;
+    _date = dt;
+    _category = ct;
+}
+
+Item::~Item()
+{
+    // do nothing
+}
+
+void Item::set_text(string x)
+{
+    _tasktext = std::move(x);
+}
+
+//this function will return the line text
+string Item::text()
+{
+    return _tasktext;
+}
+
+//this function marks off a task
+void Item::done()
+{
+    _done = true;
+}
+
+//this function unmarks a task
+void Item::notdone()
+{
+    _done = false;
+}
+
+//this function identifies if a task is done
+bool Item::is_done() const
+{
+    return _done;
+}
+
+//this function sets high priority
+void Item::highpriority()
+{
+    _priority = "3";
+}
+
+//this function sets medium priority
+void Item::mediumpriority()
+{
+    _priority = "2";
+}
+
+//this function sets low priority
+void Item::lowpriority()
+{
+    _priority = "1";
+}
+
+//this function sets no priority
+void Item::nopriority()
+{
+    _priority = "0";
+}
+
+//this function returns the priority of a specified item as a name rather than a value
+string Item::getpriority()
+{
+    string p;
+    if (_priority == "0")
+    {
+        p = "";
+    }
+    else if (_priority == "1")
+    {
+        p = "low";
+    }
+    else if (_priority == "2")
+    {
+        p = "medium";
+    }
+    else if (_priority == "3")
+    {
+        p = "high";
+    }
+    return p;
+}
+
+//this function returns the priority of a specified item
+string Item::priority()
+{
+    return _priority;
+}
+
+//this function returns the category for a task
+string Item::getcategory()
+{
+    string p;
+    if (_category == "cat0")
+    {
+        p = "none";
+    }
+    else if (_category == "cat1")
+    {
+        p = "other";
+    }
+    else if (_category == "cat2")
+    {
+        p = "home";
+    }
+    else if (_category == "cat3")
+    {
+        p = "work";
+    }
+    else if (_category == "cat4")
+    {
+        p = "entertainment";
+    }
+    return p;
+}
+
+string Item::category()
+{
+    return _category;
+}
+
+//these functions get the due date
+/*string Item::getDay() {
+    return _dateDay;
+}
+string Item::getMonth() {
+    return _dateMonth;
+}
+string Item::getYear() {
+    return _dateYear;
+}*/
+
+string Item::date() {
+    return _date;
+}
+
+
+//these functions set the category for a task
+void Item::catnone() {
+    _category = "none";
+}
+void Item::catother() {
+    _category = "other";
+}
+void Item::cathome() {
+    _category = "home";
+}
+void Item::catwork() {
+    _category = "work";
+}
+void Item::catentertainment() {
+    _category = "entertainment";
+}
+
+//these functions set the due date
+void Item::setDay(const string& day)
+{
+    while (day.length() != 2)
+        _dateMonth = "0" + day;
+}
+void Item::setMonth(const string& month)
+{
+    while (month.length() != 2)
+        _dateMonth = "0" + month;
+}
+void Item::setYear(string year)
+{
+    _dateYear = std::move(year);
+}
+
+
+
+
+
+//this changes the text to lowercase so it is easier to deal with
+string Item::str_tolower(string str)
+{
+    string temp = std::move(str);
+    for (char & i : temp)
+        i = char(tolower(i));
+    return temp;
+}
+
+bool Item::operator==(Item& other) //passing other by reference so it can be modified, faster since we passing a class
+{
+    //operator overloading for == helps compare the object text to other object of same type (item)
+    //compares lowercase text to each other
+    bool text_equal = str_tolower(_tasktext) == str_tolower(other.text());
+    bool done_equal = _done == other.is_done();
+    bool priority_equal = str_tolower(_priority) == str_tolower(other.priority());
+    return text_equal && done_equal && priority_equal;
+}
+
+/*
+bool Item::operator!=(Item& other) //passing other by reference so it can be modified, faster since we passing a class
+{
+    //operator overloading for == helps compare the object text to other object of same type (item)
+    //compares lowercase text to each other
+    bool text_equal = str_tolower(_tasktext) != str_tolower(other.text());
+    bool done_equal = _done != other.is_done();
+    bool priority_equal = str_tolower(_priority) != str_tolower(other.priority());
+    return text_equal && done_equal && priority_equal;
+}*/
