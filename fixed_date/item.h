435271badf2a2bc0fb149aadc558d272b1d9aea5
mariamabdelati <75902279+mariamabdelati@users.noreply.github.com>
2021-04-17 18:20:59 +0200

    Create item.h

---
+++ fixed_date/item.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+class Item
+{
+public:
+    Item();
+    Item(const string&);
+    ~Item();
+
+    void set_text(string x);
+    string text();
+    void done();
+    void notdone();
+    bool is_done() const;
+    void highpriority();
+    void mediumpriority();
+    void lowpriority();
+    void nopriority();
+    string getpriority();
+    string priority();
+
+    string getcategory();
+    string category();
+    void cathome();
+    void catwork();
+    void catentertainment();
+    void catother();
+    void catnone();
+
+    void setDay(const string&);
+    void setMonth(const string&);
+    void setYear(string);
+    /*string getDay();
+    string getMonth();
+    string getYear();*/
+    string date();
+
+    bool operator==(Item&);
+    //bool operator!=(Item&);
+
+    static string str_tolower(string);
+
+private:
+    string _tasktext;
+    bool _done;
+    string _priority;
+    string _dateDay;
+    string _dateMonth;
+    string _dateYear;
+    string _date;
+    string _category;
+
+};
